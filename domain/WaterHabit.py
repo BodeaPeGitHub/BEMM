573b98fc8cc9d134ac3adcbaf243b7825432f264
MaraGheorghe <mara.gheorghe@stud.ubbcluj.ro>
2021-11-20 17:18:45 +0200

    Added water habbit

--- domain/WaterHabit.py
+++ domain/WaterHabit.py
@@ -1,17 +1,32 @@
 import math
+from domain.StatusEnum import Status
 
 
 class WaterHabit:
-    def __init__(self, water_habit_id, number_of_glasses, weight):
-        self.__weight = weight
-        self.__number_of_glassed_drunk = 0
-        self.__number_of_glasses = number_of_glasses
+    def __init__(self, water_habit_id, weight):
         self.__water_habit_id = water_habit_id
-        #self.__status =
+        self.__status = Status.not_started
+        self.__number_of_glassed_drunk = 0
         self.__calculate_number_of_glasses()
+        self.__weight = weight
 
     def __calculate_number_of_glasses(self):
         self.__number_of_glasses = math.ceil((self.__weight * 2.2 * 67) // 8)
 
     def get_water_habit_id(self):
         return self.__water_habit_id
+
+    def get_status(self):
+        return self.__status
+
+    def get_number_of_glasses(self):
+        return self.__number_of_glasses
+
+    def get_number_of_glasses_drunk(self):
+        return self.__number_of_glassed_drunk
+
+    def set_status(self, status):
+        self.__status = status
+
+    def add_glass_of_water_drunk(self):
+        self.__number_of_glassed_drunk += 1
