d0ef411f025b5945d025c2bd74efe5bdc4b39679
matei birjovanu <matei.ll64@gmail.com>
2021-11-20 23:04:54 +0200

    main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from datetime import date
+from gui.ui import MyApp
+
 
 from domain.User import User
 
@@ -14,7 +16,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    user = User("mara", "Mara", "Gheorghe", date(2001, 3, 22), 55, 170)
-    print(user.get_condition())
+    ui = MyApp()
+    ui.run()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
