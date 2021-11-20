c2e8af09c6f3d47991bf8198e23a01b4f1bcdb8e
MaraGheorghe <mara.gheorghe@stud.ubbcluj.ro>
2021-11-20 17:42:13 +0200

    Matei are visual studio

--- domain/SleepingHabit.py
+++ domain/SleepingHabit.py
@@ -1,10 +1,9 @@
-import math
-from BEMM.domain.StatusEnum import Status
-import StatusEnum
+from domain.StatusEnum import Status
 from datetime import date
 
+
 class SleepingHabit:
-    def __init__(self,birth_date,sleeping_habit_id):
+    def __init__(self, birth_date, sleeping_habit_id):
         self.__status = Status.not_started
         self.__birth_date = birth_date
         self.__sleeping_habit_id = sleeping_habit_id
@@ -13,23 +12,23 @@ class SleepingHabit:
 
     def get_status(self):
         return self.__status
-    
-    def set_status(self,status):
+
+    def set_status(self, status):
         self.__status = status
 
     def get_sleeping_habit_id(self):
-        return self.__sleeping_habit_ids
+        return self.__sleeping_habit_id
 
     def get_hours_slept(self):
         return self.__number_of_hours_slept
 
-    def add_slept_time(self,hours):
+    def add_slept_time(self, hours):
         self.__number_of_hours_slept = self.__number_of_hours_slept + hours
 
-    def ___compute_sleeping_time(self):
+    def __compute_sleeping_time(self):
         age = self.__birth_date - date.today
-        if self.__age <= 13:
-            return 9,11
-        if self.__age <= 17:
-            return 8,10
-        return 7,9
+        if age <= 13:
+            return 9, 11
+        if age <= 17:
+            return 8, 10
+        return 7, 9
