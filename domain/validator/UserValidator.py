d37c96f190139d76d674c52f5864cff65404c16e
matei birjovanu <matei.ll64@gmail.com>
2021-11-21 01:01:55 +0200

    validator

--- domain/validator/UserValidator.py
+++ domain/validator/UserValidator.py
@@ -2,17 +2,17 @@ from domain.exceptions.Exceptions import ValidationException
 
 
 class UserValidator:
-    def validate(self):
+    def validate(self,user):
         error = ""
-        if self.__user.get_first_name() == "":
+        if user.get_first_name() == "":
             error += "First name is a required field.\n"
-        if self.__user.get_last_name() == "":
+        if user.get_last_name() == "":
             error += "Last name is a required field.\n"
-        if self.__user.get_username() == "":
+        if user.get_username() == "":
             error += "Username is a required field.\n"
-        if self.__user.get_height() < 120 or self.__user.get_height() > 250:
+        if user.get_height() < 120 or user.get_height() > 250:
             error += "Invalid height. It has to be between 120cm and 250cm.\n"
-        if self.__user.get_weight() < 30 or self.__user.get_weight() > 300:
+        if user.get_weight() < 30 or user.get_weight() > 300:
             error += "Invalid weight. It has to be between 30kg and 300kg.\n"
         if error != "":
             raise ValidationException(error)
