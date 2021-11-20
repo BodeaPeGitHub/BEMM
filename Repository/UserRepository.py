5fcfc5f43ff4ba4c114eab2458db1e0630e45d71
BodeaPeGit <stefan.luca.bodea@stud.ubbcluj.ro>
2021-11-20 14:50:33 +0200

    Added signature.

--- Repository/UserRepository.py
+++
@@ -1,5 +0,0 @@
-class UserRepository:
-    def __init__(self, url, username, password):
-        self.__password = password
-        self.__username = username
-        self.__url = url
---
+++ repository/UserRepository.py
@@ -0,0 +1,22 @@
+class UserRepository:
+
+    def __init__(self, url, username, password):
+        self.__password = password
+        self.__username = username
+        self.__url = url
+
+    def save(self, user):
+        pass
+
+    def update(self, user):
+        pass
+
+    def delete(self, user):
+        pass
+
+    def find_one(self, user):
+        pass
+
+    def find_all(self):
+        pass
+
